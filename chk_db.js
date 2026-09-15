9fab104cf39a5b507238f58838a63a1c56aa789e
dedo159 <dedo159@users.noreply.github.com>
2026-09-15 19:11:06 +0300

    Clean up remaining test files

--- chk_db.js
+++
@@ -1,3 +0,0 @@
-const { PrismaClient } = require('@prisma/client');
-const prisma = new PrismaClient();
-prisma.student.findFirst().then(s => console.log(s.skills)).finally(() => prisma.$disconnect());
--- chk_db2.js
+++
@@ -1,3 +0,0 @@
-const { PrismaClient } = require('@prisma/client');
-const prisma = new PrismaClient();
-prisma.student.findMany({ select: { skills: true } }).then(s => console.log(s)).finally(() => prisma.$disconnect());
